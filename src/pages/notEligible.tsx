202d82f7783caec5eced53c2273efdf895775984
Sherman28401 <sherman9804@yahoo.com>
2021-01-17 13:01:33 -0800

    commit on main

--- src/pages/notEligible.tsx
+++ src/pages/notEligible.tsx
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const NotEligible = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>We're sorry, but it appears that you are not eligible to be a plasma donor.</h1>
+    <h1>We're sorry. It appears that you are not eligible to be a plasma donor.</h1>
     <p>Don't worry though, there's always other ways to contribute!</p>
     <Link to="/">Back to Home</Link>
   </Layout>
