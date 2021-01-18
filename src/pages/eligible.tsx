71f8f40c9a96d3bab23d9b6732fd64effcb5d2ae
Angelina Vu <vuangelina72@gmail.com>
2021-01-17 17:28:17 -0800

    get request

--- src/pages/eligible.tsx
+++ src/pages/eligible.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Button from "@material-ui/core/Button"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,6 +10,11 @@ const Eligible = () => (
     <SEO title="Page two" />
     <h1>Eligible</h1>
     <p>Welcome to page 2</p>
+    <Button href="/getRequest" variant="outlined" color="primary" size="large">
+        Find donation center
+    </Button> 
+    <a href=""></a>
+    
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
