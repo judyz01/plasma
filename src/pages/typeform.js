0ccb63bda7d32d9fc47f32868324523438157373
Judy Zhang <judy.j.zhang@gmail.com>
2021-01-20 01:53:19 -0800

    customized webpack configurations to replace typeform moduel with dummy module

--- src/pages/typeform.js
+++ src/pages/typeform.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import Button from "@material-ui/core/Button"
-//if (typeof window !== "undefined") {   const typeformEmbed = require('@typeform/embed'); }
-
-
+import * as typeformEmbed from '@typeform/embed'
 class Form extends React.Component {
   constructor(props) {
     super(props);
     this.el = null;
   }
   componentDidMount() {
-    if (typeof window !== "undefined") {
-      const typeformEmbed = require('@typeform/embed');
       if (this.el) {
         typeformEmbed.makeWidget(this.el, "https://0z0g8s2ryt8.typeform.com/to/OhhvXk5p", {
           hideFooter: true,
           hideHeaders: true,
           opacity: 0,
           onSubmit: function() {
+            window.location.href = "/uploadResults"
             console.log('Typeform successfully submitted!')
           }
         });
-      }
     }
   }
   render() {
+    if (typeof window !== 'undefined') {
     return (
-      <div style={{width: '100%', height: '700px'}} />
+      <div ref={(el) => this.el = el} style={{width: '100%', height: '700px'}} />
     )
+    }
+    return null
   }
+  
 }
 
 export default Form;
+
 
